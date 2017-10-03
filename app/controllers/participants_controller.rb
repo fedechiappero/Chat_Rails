36fe9196630046cf3853d3aca80215fb24209a83
Federico Bit <fedechiappero06@gmail.com>
2017-10-02 23:16:20 -0300

    mp completo
    se controla si existe la conversacion redirigiendo a la misma o creando una nueva sino

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,15 +1,73 @@
 class ParticipantsController < ApplicationController
 
   def create#falta controlar que si el chat ya existe te mande derecho sin crearlo de nuevo
-    @chat_room = ChatRoom.new
-    @chat_room.title = User.find(params[:id]).name
-    @chat_room.user_ids = params[:id], current_user.id
 
-    if @chat_room.save
-      redirect_to chat_room_path(@chat_room)
+    @chat_room = Participant.find_by_sql(['SELECT chat_room_id FROM participants WHERE chat_room_id IN
+                                          (SELECT chat_room_id FROM participants WHERE user_id = ? AND chat_room_id IN
+                                          (SELECT chat_room_id FROM participants WHERE user_id = ? ))
+                                          GROUP BY chat_room_id HAVING count(user_id) = 2',
+                                          params[:id], current_user.id ])
+    if @chat_room.empty?
+      @chat_room = ChatRoom.new
+      @chat_room.title = User.find(params[:id]).name
+      @chat_room.user_ids = params[:id], current_user.id
+
+      if @chat_room.save
+        redirect_to chat_room_path(@chat_room)
+      else
+        redirect_to users_path
+      end
     else
-      redirect_to users_path
+      redirect_to chat_room_path @chat_room[0].chat_room_id
     end
+
+
+    # if @chat_room.count == 2
+    #   redirect_to chat_room_path @chat_room[0].chat_room_id
+    # else
+    #   @chat_room = ChatRoom.new
+    #   @chat_room.title = User.find(params[:id]).name
+    #   @chat_room.user_ids = params[:id], current_user.id
+    #
+    #   if @chat_room.save
+    #     redirect_to chat_room_path(@chat_room)
+    #   else
+    #     redirect_to users_path
+    #   end
+    # end
+
+
+    #@chat_room_usuario1 = Participant.select(:chat_room_id).where(user_id: current_user.id)
+
+    #@chat_room_usuario2 = Participant.select(:chat_room_id).where(user_id: params[:id])
+    #ChatRoom.joins(:participants).where('participants.user_id' => current_user.id.to_s).distinct.order('chat_rooms.updated_at DESC')
+
+    #render plain: @chat_room_usuario2.inspect
+
+    # @chat_room_usuario1.each do |chat1|
+    #   @chat_room_usuario2.each do |chat2|
+    #     if chat1.chat_room == chat2.chat_room
+    #       @cantidad = Participant.select(:chat_room_id).where(chat_room_id: chat1.chat_room_id).count
+    #       if @cantidad == 2
+    #         redirect_to chat_room_path @chat_room_usuario1
+    #       end
+    #     end
+    #   end
+    # end
+    #render plain: @cantidad.inspect
+    #render plain: 'no se encontro nada'.inspect
+
+
+    #desde aca crea una nueva, sin corroborar si la ya existe (para arriba es todo prueba)
+    # @chat_room = ChatRoom.new
+    # @chat_room.title = User.find(params[:id]).name
+    # @chat_room.user_ids = params[:id], current_user.id
+    #
+    # if @chat_room.save
+    #   redirect_to chat_room_path(@chat_room)
+    # else
+    #   redirect_to users_path
+    # end
   end
 
   def destroy
