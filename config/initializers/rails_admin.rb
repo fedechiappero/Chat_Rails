9c8d8a2b27ae2f18620aad9c4165cd953704df96
Federico Bit <fedechiappero06@gmail.com>
2017-11-02 17:30:57 -0300

    rails admin configurado
    falta pedir el password de nuevo cuando el admin accede a '/admin', por seguridad

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,12 +1,13 @@
 RailsAdmin.config do |config|
 
-  ### Popular gems integration
+  config.main_app_name = ["CableChat"]
 
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+    redirect_to main_app.root_path unless current_user.admin?
+  end
+  config.current_user_method(&:current_user)
 
   ## == Cancan ==
   # config.authorize_with :cancan
@@ -26,16 +27,75 @@ RailsAdmin.config do |config|
   config.actions do
     dashboard                     # mandatory
     index                         # mandatory
-    new
-    export
-    bulk_delete
+    new do
+      only User
+    end
+    #export
+    #bulk_delete
     show
-    edit
+    edit do
+      only ['User', 'ChatRoom']
+    end
     delete
-    show_in_app
 
     ## With an audit adapter, you can add:
     # history_index
     # history_show
+  end
+
+  config.model ChatRoom do
+    list do
+      field :id
+      field :title
+      field :created_at
+      field :users
+    end
+    edit do
+      field :title do
+        required true
+      end
+    end
+  end
+
+  config.model Message do
+    list do
+      field :id
+      field :user
+      field :chat_room
+      field :created_at
+    end
+  end
+
+  config.model Participant do
+    list do
+      field :id
+      field :chat_room
+      field :user
+    end
+  end
+
+  config.model User do
+    list do
+      field :id
+      field :email
+      field :sign_in_count
+      field :last_sign_in_at do
+        label "Last Login"
+      end
+      field :last_sign_in_ip do
+        label "Last ip"
+      end
+      field :active
+    end
+    edit do
+      field :email do
+        required true
+      end
+      field :password do
+        required true
+      end
+      field :admin
+      field :active
+    end
   end
 end
